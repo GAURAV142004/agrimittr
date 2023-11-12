f9550e9c7d5cd14185441321cf08d5d86cccd782
Gaurav Pawar <135757412+GAURAV142004@users.noreply.github.com>
2023-11-12 09:16:40 -0800

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,8 @@
+function signup(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Perform signup logic here (e.g., validate inputs, store user data)
+
+    // Redirect to the home page after signup
+    window.location.href = "index.html";
+}
